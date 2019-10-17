0a0e9f2a618cf15f09af303da296d84ecd4bdddf
Jonah Offitzer <joffitzer@gmail.com>
2019-10-17 00:12:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,9 @@ def pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  random_number_hash = {
+    :id => 
+  }
 end
 
 def my_hash_creator(key, value)
